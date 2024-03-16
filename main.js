2a71539fcefa732a1002d4191f4ca2dfc51a2dc3
Uzma-Abid <161968674+Uzma-Abid@users.noreply.github.com>
2024-03-16 23:50:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//print Hello World
+var message = "Hello world";
+console.log(message);
+//varible
+var fullName = "Uzma Abid";
+var age = 23;
+console.log(fullName);
+console.log(age);
+//Arithemetic operators (+, -, *, /)
+var a = 10;
+var b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+//modulus %
+console.log(a % b);
+//Exponentiation **
+console.log(Math.pow(a, b));
+//Unary operators (increment ++),(decrement--)
+console.log(++a);
+console.log(--a);
+//Assignment operators(=, +=,-=)
+var num1 = 20;
+num1 -= 22;
+console.log(num1);
+var num2 = 12;
+num2 += 15;
+console.log(num2);
+//comparison operator (==, >=, >, <=, <,)
+var c = 15;
+var d = 7;
+console.log(c == d);
+console.log(c != d);
+console.log(c >= d);
+console.log(c <= d);
+//Template literals
+var firstName = "Zubia Khan";
+var number = 39;
+var msg = "Hello my name is ".concat(firstName, " and my number is ").concat(number);
+console.log(msg);
---
+++ main.ts
@@ -0,0 +1,54 @@
+//print Hello World
+let message = "Hello world";
+console.log(message);
+
+//varible
+let fullName : string = "Uzma Abid";
+let age : number = 23;
+console.log(fullName);
+console.log(age);
+
+//Arithemetic operators (+, -, *, /)
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+//modulus %
+console.log(a % b);
+
+//Exponentiation **
+console.log(a ** b);
+
+//Unary operators (increment ++),(decrement--)
+console.log(++a);
+console.log(--a);
+
+//Assignment operators(=, +=,-=)
+let num1 :number = 20;
+num1 -=22
+console.log(num1);
+
+let num2 : number = 12;
+num2 += 15
+console.log(num2);
+
+//comparison operator (==, >=, >, <=, <,)
+let c = 15
+let d = 7
+console.log(c==d);
+console.log(c!=d);
+console.log(c>=d);
+console.log(c<=d);
+
+//Template literals
+let firstName :string = "Zubia Khan"
+let number = 39
+let msg =`Hello my name is ${firstName} and my number is ${number}`
+console.log(msg);
+
+
+
